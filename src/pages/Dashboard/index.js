0af5e529d53a6ce3b5e1ffdc888c68f1f48188be
Igor Araujo <igor@bluapp.com.br>
2019-10-21 23:31:19 -0300

    add dashboard code

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api'
 
-export default function Login(){
-    return <div/>
+
+export default function Login() {
+    const [spots, setSpots] = useState([]); //espera um array
+        
+    useEffect(() => {
+        async function loadSpots() {
+            const user_id = localStorage.getItem('user');
+            const response = await api.get('/dashboard', {
+                headers: {
+                    user_id
+                }
+            });
+            setSpots(response.data);
+        }
+        loadSpots();
+        //irá executar apenas 1 vez
+    }, []);
+
+    return (
+        <>
+            <ul className='spot-list'>
+                {spots.map(spot => (
+                    <li key={spot._id}>
+                        <header></header>
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price}</span>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
 }
