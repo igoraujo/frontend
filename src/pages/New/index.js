257ba546a18b5171b064265cb0f558ef473e3148
Igor Araujo <igor@bluapp.com.br>
2019-10-22 22:47:08 -0300

    add preview image file

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,22 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
+import api from '../../services/api'
 
 import camera from '../../assets/camera.svg'
 
 import './styles.css'
 
-export default function New() {
+export default function New({ history }) {
+    const [thumbnail, setThumbnail] = useState(null);
     const [company, setCompany] = useState('');
     const [techs, setTechs] = useState('');
     const [price, setPrice] = useState('');
 
-    function handleSubmit() {
-        
+    const preview = useMemo(() => {
+        return thumbnail ? URL.createObjectURL(thumbnail) : null
+     }, [thumbnail]);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const data = new FormData();
+        const user_id = localStorage.getItem('user');
+
+        data.append('thumbnail', thumbnail);
+        data.append('company', company);
+        data.append('techs', techs);
+        data.append('price', price);
+
+        await api.post('/spots', data, {
+            headers: { user_id }
+        });
+
+        history.push('/dashboard')
     }
 
     return (
         <form onSubmit={ handleSubmit }>
-            <label id="thumbnail">
-                <input type="file" />
+            <label id="thumbnail" 
+                style={{ backgroundImage: `url(${preview})` }}
+                className={ thumbnail ? 'has-thumbnail' : '' }
+                >
+                <input type="file" onChange={ e => setThumbnail(e.target.files[0]) } />
                 <img src={ camera } alt="Select img" />
             </label>
 
