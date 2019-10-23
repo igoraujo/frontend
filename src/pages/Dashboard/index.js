8241d059903ad233a618cb6e8d9a21ba1fea67b4
Igor Araujo <igor@bluapp.com.br>
2019-10-22 21:59:33 -0300

    updating dashboard and add upload image function

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../../services/api'
 
 import './styles.css';
 
-export default function Login() {
+export default function Dashboard() {
     const [spots, setSpots] = useState([]); //espera um array
         
     useEffect(() => {
@@ -25,12 +26,17 @@ export default function Login() {
             <ul className='spot-list'>
                 {spots.map(spot => (
                     <li key={spot._id}>
-                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }}></header>
+                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }}/>
                         <strong>{spot.company}</strong>
                         <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>
                     </li>
                 ))}
             </ul>
+
+            <Link to="/new">
+                <button className="btn">Cadastrar novo Spot</button>
+            </Link>
+
         </>
     )
 } 
--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,5 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Login(){
-    return <div/>
+import camera from '../../assets/camera.svg'
+
+import './styles.css'
+
+export default function New() {
+    const [company, setCompany] = useState('');
+    const [techs, setTechs] = useState('');
+    const [price, setPrice] = useState('');
+
+    function handleSubmit() {
+        
+    }
+
+    return (
+        <form onSubmit={ handleSubmit }>
+            <label id="thumbnail">
+                <input type="file" />
+                <img src={ camera } alt="Select img" />
+            </label>
+
+            <label htmlFor="company">EMPRESA *</label>
+            <input
+                id="company"
+                placeholder="Sua empresa incrível"
+                value={ company }
+                onChange={ e => setCompany(e.target.value) }
+            />
+            <label htmlFor="techs">TECNOLOGIAS * <span>(separadas por vírgula)</span></label>
+            <input
+                id="techs"
+                placeholder="Quais tecnologias usam?"
+                value={ techs }
+                onChange={ e => setTechs(e.target.value) }
+            />
+            <label htmlFor="price">VALOR DA DIÁRIA * <span>(em branco para GRATUITO)</span></label>
+            <input
+                id="price"
+                placeholder="Valor cobrado por dia"
+                value={ price }
+                onChange={ e => setPrice(e.target.value) }
+            />
+            <button type="submit" className="btn">Cadastrar</button>
+        </form>
+    )
 }
